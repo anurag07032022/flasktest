e3c52bc0036e821776921348623179a877774eda
anurag07032022 <101088028+anurag07032022@users.noreply.github.com>
2022-03-08 15:55:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config["DEBUG"] = True
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask using Jenkinks Trial Number:1'
+    return 'Web App with Python Flask using Jenkinks Trial Number:2'
     
 
 
